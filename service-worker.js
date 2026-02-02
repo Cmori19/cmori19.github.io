ace94db5f45dae4029d87ad942dfe8db116ea673
Cmori19 <carlos.morillas7@gmail.com>
2026-02-02 11:44:07 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // service-worker.js
-const CACHE_NAME = "planner-shell-v5-2";
+const CACHE_NAME = "planner-shell-v5-3";
 
 const APP_SHELL = [
   "./",
@@ -59,6 +59,7 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
+
 
 
 
